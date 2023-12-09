538a0b44ab5bd4dbaa6f7e244e2f12db0e277454
Filipe Carvalho <34546039+filcarv@users.noreply.github.com>
2023-12-09 13:51:17 +0000

    Added stepper control files.

---
+++ Firmware/StepperDriver/stepper_control.c
@@ -0,0 +1,2 @@
+#include "stepper_control.h"
+#include "app_ios_and_regs.h"
---
+++ Firmware/StepperDriver/stepper_control.h
@@ -0,0 +1,14 @@
+#ifndef _STEPPER_CONTROL_H_
+#define _STEPPER_CONTROL_H_
+#include <avr/io.h>
+
+// Define if not defined
+#ifndef bool
+#define bool uint8_t
+#endif
+#ifndef true
+#define true 1
+#define false 0
+#endif
+
+#endif /* _STEPPER_CONTROL_H_ */
