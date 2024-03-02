52115d42131bc98f4330e334d8448b4c74c5c0b2
Filipe Carvalho <34546039+filcarv@users.noreply.github.com>
2024-03-02 22:22:21 +0000

    Added motor stopped event to external interlock.

--- Firmware/StepperDriver/interrupts.c
+++ Firmware/StepperDriver/interrupts.c
@@ -230,15 +230,27 @@ ISR(PORTQ_INT1_vect, ISR_NAKED)
 		{
 			if (enable_counter == 0)
 			{
+				uint8_t motor_stopped_mask = 0;
+				
 				enable_counter = 1;
 				
-				clr_DRIVE_ENABLE_M0; stop_rotation(0); clr_LED_M0;
-				clr_DRIVE_ENABLE_M1; stop_rotation(1);; clr_LED_M1;
-				clr_DRIVE_ENABLE_M2; stop_rotation(2);; clr_LED_M2;
-				clr_DRIVE_ENABLE_M3; stop_rotation(3);; clr_LED_M3;
+				motor_stopped_mask |= (if_moving_stop_rotation(0)) ? B_MOTOR0 : 0;
+				motor_stopped_mask |= (if_moving_stop_rotation(1)) ? B_MOTOR1 : 0;
+				motor_stopped_mask |= (if_moving_stop_rotation(2)) ? B_MOTOR2 : 0;
+				motor_stopped_mask |= (if_moving_stop_rotation(3)) ? B_MOTOR3 : 0;
+				
+				clr_DRIVE_ENABLE_M0; clr_LED_M0;
+				clr_DRIVE_ENABLE_M1; clr_LED_M1;
+				clr_DRIVE_ENABLE_M2; clr_LED_M2;
+				clr_DRIVE_ENABLE_M3; clr_LED_M3;
 				
 				app_regs.REG_EMERGENCY_DETECTION = GM_DISABLED;
 				core_func_send_event(ADD_REG_EMERGENCY_DETECTION, true);
+				
+				if (motor_stopped_mask)
+				{
+					send_motors_stopped_event(motor_stopped_mask);
+				}
 			}
 		}
 	}
@@ -254,15 +266,27 @@ ISR(PORTQ_INT1_vect, ISR_NAKED)
 		{
 			if (enable_counter == 0)
 			{
+				uint8_t motor_stopped_mask = 0;
+				
 				enable_counter = 1;
 				
-				clr_DRIVE_ENABLE_M0; stop_rotation(0); clr_LED_M0;
-				clr_DRIVE_ENABLE_M1; stop_rotation(1); clr_LED_M1;
-				clr_DRIVE_ENABLE_M2; stop_rotation(2); clr_LED_M2;
-				clr_DRIVE_ENABLE_M3; stop_rotation(3); clr_LED_M3;
+				motor_stopped_mask |= (if_moving_stop_rotation(0)) ? B_MOTOR0 : 0;
+				motor_stopped_mask |= (if_moving_stop_rotation(1)) ? B_MOTOR1 : 0;
+				motor_stopped_mask |= (if_moving_stop_rotation(2)) ? B_MOTOR2 : 0;
+				motor_stopped_mask |= (if_moving_stop_rotation(3)) ? B_MOTOR3 : 0;
+				
+				clr_DRIVE_ENABLE_M0; clr_LED_M0;
+				clr_DRIVE_ENABLE_M1; clr_LED_M1;
+				clr_DRIVE_ENABLE_M2; clr_LED_M2;
+				clr_DRIVE_ENABLE_M3; clr_LED_M3;
 				
 				app_regs.REG_EMERGENCY_DETECTION = GM_DISABLED;
 				core_func_send_event(ADD_REG_EMERGENCY_DETECTION, true);
+				
+				if (motor_stopped_mask)
+				{
+					send_motors_stopped_event(motor_stopped_mask);
+				}
 			}
 		}
 		
